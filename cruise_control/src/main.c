500d9efe8d5a8f6f3e56e6ed9baa663a2990c953
EliasFIlho <eliasfilho202@gmail.com>
2025-04-26 01:16:10 -0300

    .

--- cruise_control/src/main.c
+++ cruise_control/src/main.c
@@ -1,27 +1,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
-#include "serial/serial_handler.h"
 #include "control/motor.h"
 #include <stdlib.h>
 #include "pid.h"
 
-#define q_msg_size 32
-
-K_MSGQ_DEFINE(uart_queue, q_msg_size, 10, 4);
 
 int main(void)
 {
-
-    char tx_buf[50];
-    if (serial_init())
-    {
-        printk("Serial started");
-    }
-    else
-    {
-        printk("Serial got some error\n");
-    }
 
     if (init_motor())
     {
@@ -42,14 +28,6 @@ int main(void)
     set_pwm_duty_period(0);
     while (1)
     {
-        if (k_msgq_get(&uart_queue, &tx_buf, K_NO_WAIT) == 0)
-        {
-            target = atoi(tx_buf);
-            if(target < 0){
-                target *= -1;
-            }
-            set_pid_target_rpm(target);
-        };
         k_sleep(K_USEC(50));
     }
 
